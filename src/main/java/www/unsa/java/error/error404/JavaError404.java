4ea368e19945414d9c603a3b638d0f4527ec0495
UNSA-Studio <UNSA-studio@outlook.com>
2026-07-16 02:07:30 +0800

    Fix dangling entrypoint: simplify main class, remove network and mixinconfig from build

--- src/main/java/www/unsa/java/error/error404/JavaError404.java
+++ src/main/java/www/unsa/java/error/error404/JavaError404.java
@@ -2,26 +2,13 @@ package www.unsa.java.error.error404;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
-import net.neoforged.neoforge.network.registration.PayloadRegistrar;
-import www.unsa.java.error.error404.item.ModDataComponents;
 import www.unsa.java.error.error404.item.ModItems;
-import www.unsa.java.error.error404.network.CrashPayload;
-import www.unsa.java.error.error404.network.DisconnectPayload;
 
 @Mod(JavaError404.MODID)
 public class JavaError404 {
-    public static final String MODID = "java.error.404";
+    public static final String MODID = "java_error_404";
 
     public JavaError404(IEventBus modEventBus) {
         ModItems.ITEMS.register(modEventBus);
-        ModDataComponents.REGISTRY.register(modEventBus);
-        modEventBus.addListener(this::onRegisterPayloads);
-    }
-
-    private void onRegisterPayloads(RegisterPayloadHandlersEvent event) {
-        PayloadRegistrar registrar = event.registrar(MODID);
-        registrar.playToClient(CrashPayload.TYPE, CrashPayload.STREAM_CODEC, CrashPayload::handle);
-        registrar.playToClient(DisconnectPayload.TYPE, DisconnectPayload.STREAM_CODEC, DisconnectPayload::handle);
     }
 }
